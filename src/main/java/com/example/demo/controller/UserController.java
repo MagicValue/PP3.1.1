c4cabe83fe34db0b3f07c7a0a25f32bd80daed86
MagicValue <sanek1023jr12@yandex.by>
2022-05-26 13:14:34 +0300

    bagFix3.0

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import com.example.demo.service.UserServiceImpl;
+
 
 import java.util.List;
 
